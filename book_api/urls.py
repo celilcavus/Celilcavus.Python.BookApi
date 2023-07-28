5804fc4267a0e8291c35728512bd3d4235af9a0e
celil çavuş <celilcavus13@hotmail.com>
2023-07-28 16:46:13 +0300

    python api tamamlandi

---
+++ book_api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.books),
+    path('<int:id>', views.book),
+]
+
+
+# localhost/books   => GET, POST
+# localhost/books/2 => GET, PUT, DELETE
